a9f400b59eee3a9139ceb6f8c3020b3e32105201
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-09-04 16:41:40 -0400

    feat: mi primera plantilla

--- bienvenida/bienvenida/views.py
+++ bienvenida/bienvenida/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Producto
+
 
 def inicio(request):
     nombre = "Sebastian Morales"
     return HttpResponse(f"¡Bienvenidos a mi primera app Django, {nombre}!")
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos': productos})
